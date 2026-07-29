b922e0ccf9c8f459858645d44974510ba467ec55
petrolopatkin <piter.lopatkin@gmail.com>
2026-07-29 20:57:18 +0200

    Day 35: modified my project "Birthday". located some objects into their directories. added some sound effects and one song.

--- birthday/birthday_v2.py
+++ birthday/birthday_v2.py
@@ -1,5 +1,15 @@
 import time
 from PIL import Image
+import pygame
+pygame.mixer.init()
+magic_appearance = pygame.mixer.Sound("birthday/sounds/magic_appearance.wav")
+magic_appearance2 = pygame.mixer.Sound("birthday/sounds/magic_appearance2.wav")
+hammer = pygame.mixer.Sound("birthday/sounds/hammer_hit.wav")
+drill =  pygame.mixer.Sound("birthday/sounds/drill_sound_new.wav")
+second_box = pygame.mixer.Sound("birthday/sounds/appearance_for_second_box.wav")
+wrong_answer = pygame.mixer.Sound("birthday/sounds/wrong_answer.wav")
+correct_answer =  pygame.mixer.Sound("birthday/sounds/correct_answer.wav")
+song = pygame.mixer.Sound("birthday/sounds/background_song.mp3")
 def slow_print(message):
    for letter in message:
       print(letter, end="")
@@ -13,9 +23,10 @@ def intro():
 Привет, Пап. Сегодня я подготовил для тебя небольшое представление в честь дня рождения 
 Нажми на ENTER, чтобы продолжить.
 ========================================================================================"""
-    image1 = Image.open('birthday/test.jpg')
+    image1 = Image.open('birthday/pictures/test.jpg')
     slow_print(intro_message)
     time.sleep(1)
+    magic_appearance.play()
     image1.show()
     input("Нажми ENTER чтобы продолжить ")
     time.sleep(2)
@@ -28,16 +39,18 @@ def first_scene():
 1. Открыть сразу
 2. Потрясти коробку
 ============================================="""
-     box_image = Image.open("birthday/birthday_box.jpg")
+     box_image = Image.open("birthday/pictures/birthday_box.jpg")
      slow_print(first_scene_message)
      time.sleep(1)
+     magic_appearance2.play()
      box_image.show()
      try:
       choice = int(input("> "))
      except ValueError:
-        print("Invalid value, try again")
+        print("Ты ввел несуществующий вариант, попробуй снова")
         continue
      if choice == 1:
+      wrong_answer.play()
       choice1 = """
 Ты слишком торопишься. 
 Так представление будет не интересным.
@@ -45,6 +58,7 @@ def first_scene():
 """
       slow_print(choice1)
      elif choice == 2:
+      correct_answer.play()
       choice2 = """
 Ты потряс коробку...
 Внутри ощущаеться что-то тяжелое
@@ -72,15 +86,18 @@ def second_scene():
         try:
             choice = int(input("> "))
         except ValueError:
-            print("Invalid value, try again")
+            print("Ты ввел несуществующий вариант, попробуй снова")
             continue
         if choice == 1:
+            wrong_answer.play()
             open_box = "Все еще слишком рано! Попробуй выбрать другой вариант."
             slow_print(open_box)
         elif choice == 2:
+            wrong_answer.play()
             try_to_look_into = "Внутри коробки слишком темно, ничего не видно."
             slow_print(try_to_look_into)
         elif choice == 3:
+            correct_answer.play()
             continue_show = "Отлично, правильный выбор. Продолжаем представление."
             slow_print(continue_show)
             time.sleep(2)
@@ -103,7 +120,7 @@ def third_scene():
       try:
          choice = int(input("> "))
       except ValueError:
-         print("Invalid value, try again")
+         print("Ты ввел несуществующий вариант, попробуй снова")
          continue
       if choice == 1:
          choice1 = """Ты выбрал молоток.
@@ -111,20 +128,22 @@ def third_scene():
 Вместе с подарком..
 Попробуй выбрать другой вариант
 """
-         crushed_box1 = Image.open("birthday/crushed_box1.jpg")
+         crushed_box1 = Image.open("birthday/pictures/crushed_box1.jpg")
          slow_print(choice1)
          time.sleep(1)
+         hammer.play()
          crushed_box1.show()
       elif choice == 2:
          choice2 = """Ты выбрал дрель
-Ты попробовать просверлить дырку в коробке..
+Ты попробовал просверлить дырку в коробке..
 Но не расчитал силы и пробил ее..
 Вместе с подарком и столом..
 Я думаю стоит попробовать другой вариант
 """
-         crushed_box2 = Image.open("birthday/crushed_box2.jpg")
+         crushed_box2 = Image.open("birthday/pictures/crushed_box2.jpg")
          slow_print(choice2)
          time.sleep(1)
+         drill.play()
          crushed_box2.show()
       elif choice == 3:
          choice3 = """Ты выбрал универсальную открывашку.
@@ -132,9 +151,10 @@ def third_scene():
 И внутри..
 Еще одна коробка;)
 """
-         box_in_box = Image.open("birthday/box_in_box.jpg")
+         box_in_box = Image.open("birthday/pictures/box_in_box.jpg")
          slow_print(choice3)
          time.sleep(1)
+         magic_appearance.play()
          box_in_box.show()
          time.sleep(2)
          break
@@ -153,22 +173,26 @@ def fourth_scene():
 3. Открыть руками
 =======================================================
 """
-    small_box = Image.open("birthday/small_box.jpg")
+    small_box = Image.open("birthday/pictures/small_box.jpg")
     slow_print(fourth_scene_message)
     time.sleep(1)
+    second_box.play()
     small_box.show()
     try:
        choice = int(input("> "))
     except ValueError:
-       print("Invalid value, try again")
+       print("Ты ввел несуществующий вариант, попробуй снова")
        continue
     if choice == 1:
+       wrong_answer.play()
        choice1 = "В этот раз открывашка не подходит. Попробуй другой вариант"
        slow_print(choice1)
     elif choice == 2:
+       wrong_answer.play()
        choice2 = "Предыдущий опыт тебя ничему не учит? Ты снова сломал коробку. Попробуй другой вариант"
        slow_print(choice2)
     elif choice == 3:
+       correct_answer.play()
        choice3 = "Небольшой троллинг)) С этой коробкой все намного проще чем с первой"
        slow_print(choice3)
        time.sleep(2)
@@ -180,6 +204,7 @@ def fourth_scene():
 
 def last_scene():
    while True:
+    song.play()
     last_scene_message1 = """==============================================================================================
 Поздравляю, ты дошел до финальной сцены моего небольшого шоу
 Надеюсь это было хоть чуточку весело, потому что мне было очень сложно придумать что-то самому
@@ -191,13 +216,14 @@ def last_scene():
 
     last_scene_message2 = """===============================================================================================================================================
 Пап, поздравляю тебя с днем рождения!
-Желаю, чтобы в жизни было побольше радостных моментов
-Чтобы здоровье не подводило тебя в важные моменты
-Чтобы получалось все задуманное, а все мечты ставали целями
-Ты сделал очень много в жизни для меня и для нашей семьи в целом, спасибо тебе большое
-Надеюсь, что в ближайших пару лет я смогу вам помогать, чтобы вы с мамой просто сидели дома и отдыхали
+Желаю, чтобы в жизни было побольше радостных моментов.
+Чтобы здоровье не подводило тебя в важные моменты.
+Чтобы получалось все задуманное, а все мечты ставали целями.
+Ты сделал очень много в жизни для меня и для нашей семьи в целом, спасибо тебе большое.
+Надеюсь, что в ближайших пару лет я смогу вам помогать, чтобы вы с мамой просто сидели дома и отдыхали.
 Спасибо, что сыграл в мою маленькую игру, я правда над ней старался.
-Можешь считать, что в нее вложены очень многие знания, которые я получил за последний месяц занятий.
+Можешь считать, что в нее вложены очень многие знания, которые я получил за время моих занятий.
+Пока мне нравиться это дело и я планирую продолжать развиваться в этой сфере.
 Если у тебя появятся идеи как сделать эту игру ещё лучше — обязательно расскажи мне. 
 Мне очень интересно развиваться и делать проекты более качественными.
 Еще раз с днем рождения и хорошего дня!
@@ -215,7 +241,7 @@ while True:
     try:
         command = int(input("> "))
     except ValueError:
-        print("Incorrect value, try again")
+        print("Ты ввел несуществующий вариант, попробуй снова")
         continue
     if command == 1:
         intro()
@@ -227,4 +253,4 @@ while True:
     elif command == 2:
         break
     else:
-        print("Invalid command, try again")
+        print("Ты ввел неправильную команду, попробуй снова")
--- birthday/test.py
+++ birthday/test.py
@@ -1,5 +1,6 @@
-from PIL import Image
-import os
-print(os.listdir())
-image1 = Image.open('test.jpg')
-image1.show()
+#from PIL import Image
+#import os
+#print(os.listdir())
+#image1 = Image.open('test.jpg')
+#image1.show()
+
