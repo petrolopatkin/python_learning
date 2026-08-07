1c280bfd97d6965d041c2eaede481f89a1743e53
petrolopatkin <piter.lopatkin@gmail.com>
2026-08-07 16:57:31 +0200

    Day 44: Expense Tracker v1.0 finished

--- My projects/Expense Tracker/main.py
+++ My projects/Expense Tracker/main.py
@@ -7,6 +7,8 @@ def clear():
 
 def show_expenses():
     with open("My projects/Expense Tracker/tracker.txt", "r") as file:
+        if not line:
+            print("There are no expenses yet")
         for line in file:
             line = line.strip()
             date, category, item, price = line.split("|")
@@ -18,21 +20,62 @@ Price: {price}""")
 
 
 def add_expense():
-    date = int(input("Enter a date(DD-MM-YYYY): "))
+    date = input("Enter a date(DD-MM-YYYY): ")
     category = input("Category: ")
     item = input("Item: ")
-    price = int(input("Price: "))
+    price = float(input("Price: "))
     with open("My projects/Expense Tracker/tracker.txt", "a") as file:
         file.write(f"{date}|{category}|{item}|{price}\n")
     print("Expense was successfully added")
 
 
 def total_spent():
-    pass
+    total_price = 0
+    with open("My projects/Expense Tracker/tracker.txt", "r") as file:
+        spent = file.readlines()
+        for line in spent:
+            date, category, item, price = line.split("|")
+            print(f"""================================
+Date: {date}
+Category: {category}
+Item: {item}
+Price: {price}""")
+            price = float(price)
+            total_price += price
+        print(f"Total spent: {total_price}")
 
 
 def delete_expense():
-    pass
+    with open("My projects/Expense Tracker/tracker.txt", "r") as file:
+        expenses = file.readlines()
+        if not expenses:
+            print("There are no expences yet")
+            return
+        number = 1
+        for expense in expenses:
+            print(f"{number}. {expense}")
+            number += 1
+        del_expense = int(input("Which expense you want to delete? "))
+        if del_expense < 1 or del_expense > len(expenses):
+            print("Invalid expense")
+            return
+        while True:
+            warning_message = input("Are you sure you want to delete this expense? ").lower()
+            if warning_message == "y":
+                expenses.pop(del_expense - 1)
+                break
+            elif warning_message == "n":
+                print("You have cancelled a deletion")
+                return
+            else:
+                print("You picked incorrect answer, try again")
+                continue
+        with open("My projects/Expense Tracker/tracker.txt", "w") as file:
+            for expense in expenses:
+                file.write(expense)
+    show_expenses()
+        
+        
 
 
 while True:
