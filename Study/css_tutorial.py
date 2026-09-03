1bf72bb71694dc90e0b717469ecad629f83e4425
petrolopatkin <piter.lopatkin@gmail.com>
2026-09-03 13:42:30 +0200

    Day 71 of my Python journey: modified a menu in my django project. started a css course for beginners

---
+++ Study/css_tutorial.py
@@ -0,0 +1,6 @@
+# font-size stands for how big or small is font on uor page
+# color stands for changing a color of text
+# font-style stands for the style of your font(for example italic)
+# text-transform transforms our text into uppercase, lowercase etc.
+# background-color stand for a colo of the background area(of text, body, entire webpage etc.)
+# * to select all information from the page and change everything
