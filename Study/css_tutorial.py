4592a7c19f5398807f8eda0447571d86cc09570e
petrolopatkin <piter.lopatkin@gmail.com>
2026-09-04 16:17:59 +0200

    Day 72 pf my Python journey: continue learning CSS: Box Model, Typography, Styling Links, Lists Style

--- Study/css_tutorial.py
+++ Study/css_tutorial.py
@@ -4,3 +4,55 @@
 # text-transform transforms our text into uppercase, lowercase etc.
 # background-color stand for a colo of the background area(of text, body, entire webpage etc.)
 # * to select all information from the page and change everything
+""" /* p {
+    font-size: 20px;
+    color: purple;
+}
+body {
+    font-size: 22px;
+}
+.gray {
+    color: gray;
+}
+#third {
+    font-style: italic;
+}
+h2, h3 {
+    color: blanchedalmond;
+}
+.highlight {
+    text-transform: uppercase;
+    background-color: gold;
+} */
+/* body {
+    font-size: 22px;
+    font-family: 'Times New Roman', Times, serif;
+    line-height: 1.5;
+    background-color: peru;
+    color: olive;
+} */ 
+/* 
+#Box Styling
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.container {
+    border: 10px double skyblue;
+    font-size: 1.5rem;
+    margin: 1.5em;
+    padding: 1.5em;
+    outline: 5px solid pink;
+    outline-offset: -20px;
+}
+.circle {
+    margin: 3rem auto;
+    background-color: gold;
+    width: 100px;
+    height: 100px;
+    border: 2px solid black;
+    border-radius: 50px;
+    outline: 2px solid pink;
+    outline-offset: 0.25rem;
+} */"""
