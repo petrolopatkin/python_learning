4ff9e338a809eaf2d685c9fa1e1187ed2d5b5e0a
petrolopatkin <piter.lopatkin@gmail.com>
2026-09-05 15:20:15 +0200

    Day 73 of my Python journey: continue learning CSS: mini project, display, floats, columns

--- Study/css_tutorial.py
+++ Study/css_tutorial.py
@@ -55,4 +55,185 @@ h2, h3 {
     border-radius: 50px;
     outline: 2px solid pink;
     outline-offset: 0.25rem;
-} */"""
+} 
+*/
+/* input, button {
+    font-size: inherit;
+} */
+/* p {
+   /*  line-height: 1.5; */
+    /* letter-spacing: 0.1em;
+    word-spacing: 0.25em; */
+   /*  font-weight: 700;
+    font-style: italic; */
+/* a {
+   color: #000;
+}
+a:visited {
+    color: red;
+}
+a:hover a:focus {
+    color: aquamarine;
+}
+a:active {
+    color: firebrick;
+} */
+
+/* ol {
+    list-style-type: upper-latin;
+}
+ul {
+    text-align: center;
+    line-height: 1.6;
+    list-style:square url('../img/checkmark.png') inside;
+}
+
+ul ::marker {
+    color: #000;
+    font-family: fantasy;
+    font-size: 1em;
+    content: 'Only 5$>> ';
+} */
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body {
+    margin: 0.5rem;
+    font-size: 1.5rem;
+    text-align: center;
+    font-family: "Oswald", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 500;
+    font-style: normal
+}
+
+nav{
+    border: 2px solid #333;
+    border-radius: 2rem;
+    margin: 0 auto 1rem;
+    width: 600px;
+    font-size: 3rem;
+    line-height: 7rem;
+}
+
+h2 {
+    padding: 1rem;
+    background-color: gold;
+    border-radius: 2rem 2rem 0 0;
+}
+
+ul {
+    list-style-type: none;
+}
+
+li {
+    border-top: 1px solid #333;
+}
+
+li a {
+    display: block;
+}
+
+li a,
+li a:visited{
+    text-decoration: none;
+    color: darkblue;
+}
+
+li a:hover,
+li a:focus {
+    background: #333;
+    color: whitesmoke;
+    cursor: pointer;
+}
+
+li:last-child a {
+    border-radius: 0 0 2rem 2rem;
+}
+#
+/* * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body {
+    font-family: "Oswald", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 500;
+    font-style: normal;
+    font-size: 2rem;
+} */
+
+/* p {
+    background-color: lightgray;
+}
+
+.opposite {
+    display: inline-block;
+    background-color: gold;
+    color: whitesmoke;
+    padding: 4rem;
+} */
+
+/* ul {
+    list-style-type: none;
+    padding: 0.5rem;
+    text-align: right;
+    background-color: lightgray;
+    margin: 0;
+}
+
+li {
+    display: inline-block;
+    margin-inline: 0.5rem;
+}
+
+li a {
+    color: darkmagenta;
+}
+
+li a:visited {
+    color: goldenrod;
+}
+
+li a:hover, 
+li a:focus {
+    color: tomato;
+} */
+# 
+body {
+    font-family: "Oswald", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 500;
+    font-style: normal;
+    font-size: 2rem;
+}
+.block {
+    width: 30vw;
+    height: 30vw;
+    background-color: #000;
+    color: white;
+    padding: 1.5rem;
+}
+
+.left {
+    float: left;
+    margin-right: 1rem;
+}
+.right {
+    float: right;
+    margin-left: 1rem;
+}
+
+.clear {
+    clear: both;
+}
+
+section {
+    background-color: bisque;
+    border: 1px solid #333;
+    padding: 1rem;
+    display: flow-root;
+}"""
