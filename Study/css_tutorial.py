60a625995575d3382a46b49e967503e143446e3d
petrolopatkin <piter.lopatkin@gmail.com>
2026-09-06 15:29:23 +0200

    Day 74 of my Pytho  journey: continue learning css: position, flexbox, grid layout

--- Study/css_tutorial.py
+++ Study/css_tutorial.py
@@ -236,4 +236,175 @@ section {
     border: 1px solid #333;
     padding: 1rem;
     display: flow-root;
-}"""
+}
+#
+/* .columns {
+     column-count: 4;
+    column-width: 250px;
+    columns: 4 250px;
+    column-rule: 3px solid black;
+}
+
+.columns p {
+    margin-top: 0;
+}
+
+.columns h2 {
+    margin-top: 0;
+    background-color: #333;
+    color: whitesmoke;
+    padding: 1rem;
+    break-inside: avoid;
+}
+
+.columns .quote {
+    margin-top: 2rem;
+    font-size: 2rem;
+    text-align: center;
+    color: #333;
+    column-span: all;
+}
+
+.nowraped {
+    white-space: nowrap;
+} 
+#
+.outer-container {
+    border: 3px dashed black;
+    width: 75vw;
+    height: 85vh;
+    margin: 40px auto;
+}
+
+.inner-container {
+    border: 2px solid darkblue;
+    width: 40vw;
+    height: 50vh;
+    margin: 200px auto;
+}
+
+.box {
+    width: 150px;
+    height: 150px;
+    color: #fff;
+    padding: 1rem;
+}
+
+.absolute {
+    background-color: gold;
+    position: absolute;
+    top: 0;
+    left: 0;
+}
+
+.relative {
+    background-color: olive;
+    position: relative;
+    top: 300px;
+    left: 100px;
+}
+
+.fixed {
+    background-color: darkorchid;
+    position: fixed;
+    top: 100px;
+}
+
+.sticky {
+    background-color: black;
+    position: sticky;
+    top: 0;
+}
+
+section {
+    height: 100vh;
+}
+
+.blue {
+    background-color: blue;
+}
+.red {
+    background-color: red;
+}
+.green {
+    background-color: green;
+}
+
+header, footer {
+    color: #fff;
+    text-align: center;
+    height: 100px;
+    font-size: 5rem;
+}
+
+header {
+    position: sticky;
+    top: 0;
+}
+footer {
+    background-color: #000;
+    position: fixed;
+    bottom: 0;
+}
+
+a:visited {
+    color: #fff;
+}
+
+.social {
+    background-color: royalblue;
+    color: #fff;
+    font-size: inherit;
+    padding: 1rem;
+    position: fixed;
+    top: 30%;
+    left: 0;
+}
+#
+/* * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body {
+    font-family: "Oswald", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 500;
+    font-style: normal;
+    font-size: 2rem;
+    min-height: 200vh;
+    padding: 20px;
+}
+
+.container {
+    max-width: 800px;
+    min-height: 400px;
+    margin-inline: auto;
+    border: 1px solid #000;
+    display: flex;
+    gap: 1rem;
+    justify-content: center;
+    align-items: center;
+    flex-flow: row-reverse wrap;
+    align-content: space-evenly;
+}
+
+.box {
+    min-width: 100px
+    height: 100px;
+    background-color: #000;
+    color: #fff;
+    font-size: 3rem;
+    padding: 0.5rem;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    flex: 1 1 250px;
+}
+
+.box:nth-child(2) {
+    flex: 2 2 250px;
+    order: 0;
+} """
