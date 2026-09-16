b08ddb95e0db9be40c3b00bf466a07bdd27ec9ac
petrolopatkin <piter.lopatkin@gmail.com>
2026-09-16 19:08:21 +0200

    Day 83 of my Python journey: modified my django project

--- Study/django_learning/weather/views.py
+++ Study/django_learning/weather/views.py
@@ -16,13 +16,22 @@ def index(request):
     city = ""
 
     if request.method == 'POST':
-        form = CityForm(request.POST)
+        action = request.POST.get("action")
+        city  = request.POST.get("city")
+        if action == "delete":
+            if city in cities:
+                cities.remove(city)
+                request.session["cities"] = cities
+            else:
+                print("Incorrect deletion")
+        elif action == "weather":
+            print("WEATHER CITY: ", city)
 
         if form.is_valid():
             city = form.cleaned_data["city"].strip()
 
-            if city not in cities:
-                cities.append(city)
+            if not any(saved_city.lower() == city.lower() for saved_city in cities):
+                cities.append(city).lower()
                 request.session["cities"] = cities
         else:
             print("You entered invalid city")
