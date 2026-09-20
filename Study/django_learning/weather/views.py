91c52391d31c82a9254668273a25a85fc08cc233
petrolopatkin <piter.lopatkin@gmail.com>
2026-09-20 18:43:44 +0200

    Day 86 of my Python journey: modified my django project/ added first API into my project

--- Study/django_learning/weather/views.py
+++ Study/django_learning/weather/views.py
@@ -3,15 +3,20 @@ from django.shortcuts import redirect
 from django.contrib import messages
 from .forms import CityForm
 from .models import SavedCity
+import requests
 
 def index(request):
     cities = SavedCity.objects.all()
     city = ""
     action = ""
+    temperature = ""
+    relative_humidity = ""
+    weather_code = ""
 
     form = CityForm()
 
     if request.method == 'POST':
+            print("POST RECIEVED")
             action = request.POST.get("action")
             city  = request.POST.get("city")
             if action == "delete":
@@ -22,9 +27,48 @@ def index(request):
                 else:
                     messages.success(request, "City not found")
             elif action == "weather":
-                print("WEATHER CITY: ", city)
+                print("ACTION: " ,action)
+                print("CITY: ", city)
+
+                url = "https://geocoding-api.open-meteo.com/v1/search"
+                parameters = {
+                     "name": city
+                }
+                r = requests.get(url, params=parameters)
+
+                print(r.status_code)
+                print(r.json())
+
+                data = r.json()
+
+                latitude = data["results"][0]["latitude"]
+                longitude = data["results"][0]["longitude"]
+
+                print("Latitude:", latitude)
+                print("Longitude: ", longitude)
+
+                weather_url = "https://api.open-meteo.com/v1/forecast"
+                weather_parameters = {
+                     "latitude": latitude,
+                     "longitude": longitude,
+                     "current": "temperature_2m,wind_speed_10m,wind_direction_10m,wind_gusts_10m,apparent_temperature,relative_humidity_2m,weather_code"
+                }
+
+                weather_r = requests.get(weather_url, params=weather_parameters)
+
+                print(weather_r.status_code)
+                weather_data = weather_r.json()
+                print(weather_data)
+                current = weather_data["current"]
+                temperature = current["temperature_2m"]
+                relative_humidity = current["relative_humidity_2m"]
+                weather_code = current["weather_code"]
+                wind_speed = current["wind_speed_10m"]
+                wind_direction = current["wind_direction_10m"]
+                wind_gusts = current["wind_gusts_10m"]
+                apparent_temperature = current["apparent_temperature"]
             elif action == "weather-search":
-                 message = f"You looked for {city}"
+                 messages.success(request, f"You looked for {city}")
                  form = CityForm(request.POST)
                  if form.is_valid():
                         city = form.cleaned_data["city"].strip()
@@ -45,6 +89,13 @@ def index(request):
         "cities": cities,
         "city": city,
         "form": form,
-        "action": action
+        "action": action,
+        "temperature": temperature,
+        "relative_humidity": relative_humidity,
+        "weather_code": weather_code,
+        "wind_speed": wind_speed,
+        "wind_direction": wind_direction,
+        "wind_gusts": wind_gusts,
+        "apparent_temperature": apparent_temperature
         }
     )
